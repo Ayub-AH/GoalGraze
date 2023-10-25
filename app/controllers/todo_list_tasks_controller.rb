00bd5ac654c64a52a0286ee5fd5affd0c54ccc43
Ayub Hassan <ayyubahmed555@gmail.com>
2023-10-25 20:36:22 +0100

    fixed completd tasks feature and added styling on main pages

--- app/controllers/todo_list_tasks_controller.rb
+++ app/controllers/todo_list_tasks_controller.rb
@@ -5,6 +5,7 @@ class TodoListTasksController < ApplicationController
   def index
     @todo_list_tasks = current_user.todo_list_tasks
     @todo_list_task = current_user.todo_list_tasks.build
+    @incomplete_tasks = current_user.todo_list_tasks.where(completed: false)
   end
 
   # GET /todo_list_tasks/1 or /todo_list_tasks/1.json
@@ -20,20 +21,26 @@ class TodoListTasksController < ApplicationController
   end
 
   # POST /todo_list_tasks or /todo_list_tasks.json
-  def create
-    @todo_list_task = TodoListTask.new(todo_list_task_params)
-    @todo_list_task.user = current_user
+# POST /todo_list_tasks or /todo_list_tasks.json
+def create
+  @todo_list_task = TodoListTask.new(todo_list_task_params)
+  @todo_list_task.user = current_user
+  permitted_params = params.require(:todo_list_task).permit(:title, :description, :due_date)
+  @todo_list_task.attributes = permitted_params
 
-    respond_to do |format|
-      if @todo_list_task.save
-        format.html { redirect_to todo_list_task_url(@todo_list_task), notice: "Todo list task was successfully created." }
-        format.json { render :show, status: :created, location: @todo_list_task }
-      else
-        format.html { render :index, status: :unprocessable_entity }
-        format.json { render json: @todo_list_task.errors, status: :unprocessable_entity }
-      end
+  respond_to do |format|
+    if @todo_list_task.save
+      format.html { redirect_to todo_list_task_url(@todo_list_task), notice: "Todo list task was successfully created." }
+      format.json { render :show, status: :created, location: @todo_list_task }
+    else
+      # If the creation fails, render the :index view with validation errors.
+      # You can set @todo_list_tasks here if you need it in your view.
+      @todo_list_tasks = current_user.todo_list_tasks
+      format.html { render :index, status: :unprocessable_entity }
+      format.json { render json: @todo_list_task.errors, status: :unprocessable_entity }
     end
   end
+end
 
   # PATCH/PUT /todo_list_tasks/1 or /todo_list_tasks/1.json
   def update
@@ -48,13 +55,15 @@ class TodoListTasksController < ApplicationController
     end
   end
 
-  def move_to_completed
-    @todo_list_task = TodoListTask.find(params[:id])
-    @todo_list_task.update(completed: true)
-
-    # Optionally, you can redirect to a completed items page or do other actions here.
+  def complete_task
+    todo_list_task = TodoListTask.find(params[:id])
+    todo_list_task.update(completed: true)
+    redirect_to completed_tasks_path
   end
-
+  def completed_tasks
+    @completed_tasks = current_user.todo_list_tasks.where(completed: true)
+    # You can also add any other logic you need for displaying completed tasks.
+  end
   # DELETE /todo_list_tasks/1 or /todo_list_tasks/1.json
   def destroy
     if
@@ -77,6 +86,6 @@ class TodoListTasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_list_task_params
-      params.require(:todo_list_task).permit(:title, :description, :due_date, :completed).merge(user_id: current_user.id)
+      params.require(:todo_list_task).permit!.merge(user_id: current_user.id)
     end
 end
